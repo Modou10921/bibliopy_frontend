8118b5190327e1028be75022427f90199c155bc9
Modou10921 <oluodom418@gmail.com>
2026-08-27 19:57:46 +0000

    Fix: import unique AuthService dans inscription.ts

--- src/app/public/inscription/inscription.ts
+++ src/app/public/inscription/inscription.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { CommonModule, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { AuthService } from '../../services/auth.service'; // Ajustez le chemin vers auth.service si nécessaire
+import { AuthService } from '../../../core/services/auth';
 
 @Component({
   selector: 'app-inscription',
