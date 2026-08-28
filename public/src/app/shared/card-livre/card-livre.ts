246c549a23f4b2d35942fe83a12a565748feec2b
Modou10921 <oluodom418@gmail.com>
2026-08-28 13:39:19 +0000

    Nettoyage des fichiers inutiles

--- public/src/app/shared/card-livre/card-livre.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-card-livre',
-  imports: [],
-  templateUrl: './card-livre.html',
-  styleUrl: './card-livre.css',
-})
-export class CardLivre {}
